909ba1920b2d9d4b8b394dd9909f4e1369f7acae
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2023-04-16 00:30:19 +0200

    SPRIN-12 - create react lists using common list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import { SplitScreen } from './SplitScreen/SplitScreen';
+import { SmallPersonListItem } from './Lists/Person/SmallPersonListItem';
+import { RegularList } from './Lists/RegularList';
+import { LargePersonListItem } from './Lists/Person/LargePersonListItem';
+
+const people = [{
+  name: 'Nadee Sansari',
+  age: 28,
+  hairColor: 'black',
+  hobbies: ['dancing', 'reading', 'movie watching']
+}, {
+  name: 'Shan Kumar',
+  age: 48,
+  hairColor: 'brown',
+  hobbies: ['bicycling', 'reading', 'swimming']
+}, {
+  name: 'Nilz Peter',
+  age: 18,
+  hairColor: 'white',
+  hobbies: ['bicycling', 'video games', 'swimming']
+}]
 
 const LeftHandComponent = ({name}) => {
-  return <h1 style={{backgroundColor: 'green'}}>{name}</h1>;
+  return <>
+            <h1 style={{backgroundColor: 'green'}}>{name} </h1>
+            <p style={{color: 'purple'}}><b>Small List Item</b></p>
+            <RegularList 
+              items={people}
+              resourceName="person"
+              itemComponent={SmallPersonListItem}/>
+
+            <br />
+            <p style={{color: 'purple'}}><b>Large List Item</b></p>
+            <RegularList 
+              items={people}
+              resourceName="person"
+              itemComponent={LargePersonListItem}/>
+          </>
+         ;
 }
 
 const RightHandComponent = ({message}) => {
@@ -13,8 +47,8 @@ const RightHandComponent = ({message}) => {
 function App() {
   return (
     <SplitScreen leftWeight={1} rightWeight={3}>
-        <LeftHandComponent name="Nadee Sansari"/> 
-        <RightHandComponent message="Try your best"/>
+        <LeftHandComponent name="Left Hand Title"/> 
+        <RightHandComponent message="Right Hand Title"/>
     </SplitScreen>
   );
 }
